c85e4eee74c0799f1f29787fbee252bcaa1a2496
Joseph Kim <josepheh.kim@gmail.com>
2019-08-16 13:55:09 -0400

    started the card component

--- client/src/component/Graph1Page/Graph1Page.js
+++ client/src/component/Graph1Page/Graph1Page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
-import Graph1 from '../Graphs/Graph1'
+import Graph1 from '../Graphs/Graph1';
+import Card from '../Card/Card.js';
 
 class Graph1Page extends React.Component {
   render(){
@@ -8,6 +9,7 @@ class Graph1Page extends React.Component {
       <>
         <NavBar />
         <Graph1/>
+        <Card />
       </>
     )}
 }
