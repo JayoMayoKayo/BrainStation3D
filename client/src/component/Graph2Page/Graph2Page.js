40073eb812ce3ff812a34f2f3307a1b5ded47320
Omar Khan <o.khandxb@gmail.com>
2019-08-16 12:51:51 -0400

    graphs added

--- client/src/component/Graph2Page/Graph2Page.js
+++ client/src/component/Graph2Page/Graph2Page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
+import Graph2 from '../Graphs/Graph2'
+
 
 class Graph2Page extends React.Component {
   render(){
     return(
       <>
         <NavBar />
-        <h1>Graph 2 Page</h1>
+        <Graph2/>
       </>
     )}
 }
